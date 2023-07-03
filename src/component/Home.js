4e4ac13a36c8defef309f2ed51d9113cafd3fbc2
Maha-Engg <mahacm98@gmail.com>
2023-07-03 14:48:08 +0530

    hi

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import img from './img.png'
+import {Link} from 'react-scroll'
+
+
 
 function Home() {
   return (
@@ -9,8 +12,8 @@ function Home() {
         <h3> Hello,</h3>
         <h2> I'M Mahalakshmi</h2>
         <h4> Full stack Developer Fresher</h4>
-        <button className='hire-btn btn'> Hire Me</button>
-        <button className='down-cv btn '> Download CV</button>
+        <Link to='cont' className='hire-btn btn'> Hire Me</Link>
+       <a  href="./myresume.pdf" className='down-cv btn '> Download CV</a>
     </div>
     <img  src={img} className='my-img' alt='...'/>
     </section>
